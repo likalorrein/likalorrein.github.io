454d4ed3770ac1c2d2b2cd29f6cff5ccfcb38ad6
likalorrein <144332456+likalorrein@users.noreply.github.com>
2023-10-12 12:46:39 +0300

    Add files via upload

---
+++ recipe/lib/script.js
@@ -0,0 +1,31 @@
+<head>
+<title> Pfannkuchensuppe</title>
+</head>
+für 2 Portionen
+2 Bund
+Suppengemüse
+2
+Zwiebeln
+½ Knolle
+Fenchel
+1
+Lorbeerblatt
+3
+Nelken
+1 TL
+Pfefferkörner
+3
+Wacholderbeeren
+Salz
+Pfeffer aus der Mühle
+Für die Pfannkuchen
+2
+Eier
+100 g
+Mehl
+200 ml
+Milch
+Butter
+3 EL
+Schnittlauchröllchen
+
